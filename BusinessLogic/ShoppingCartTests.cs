edb7cdaa8c1836036137b5280c299fd6999d4968
Olaf Thielke <olaf.thielke@gmail.com>
2021-12-03 11:56:10 +1300

    Can Create ShoppingCart - Red (Compile)

---
+++ BusinessLogic/ShoppingCartTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace BusinessLogic
+{
+    public class ShoppingCartTests
+    {
+        [Fact]
+        public void Can_Create_ShoppingCart()
+        {
+            var cart = new ShoppingCart();
+        }
+    }
+}
