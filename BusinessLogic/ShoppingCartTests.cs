4f4663c4c9266a59a79df27854225f4078bf2adf
Olaf Thielke <olaf.thielke@gmail.com>
2021-12-20 11:11:49 +1300

    Turn Negative Quantity Test Into Theory - Refactor

--- BusinessLogic/ShoppingCartTests.cs
+++ BusinessLogic/ShoppingCartTests.cs
@@ -41,20 +41,15 @@ namespace BusinessLogic
             add.Should().ThrowExactly<ZeroQuantity>().WithMessage("Zero is not a valid quantity.");
         }
 
-        [Fact]
-        public void Given_Quantity_Is_Negative_1_When_Call_Add_Then_Throw_NegativeQuantity_Exception()
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-3)]
+        [InlineData(-20)]
+        public void Given_Quantity_Is_Negative_When_Call_Add_Then_Throw_NegativeQuantity_Exception(int quantity)
         {
             var cart = new ShoppingCart();
-            Action add = () => cart.Add(new Product(), -1);
-            add.Should().ThrowExactly<NegativeQuantity>().WithMessage("-1 is an invalid quantity.");
-        }
-
-        [Fact]
-        public void Given_Quantity_Is_Negative_3_When_Call_Add_Then_Throw_NegativeQuantity_Exception()
-        {
-            var cart = new ShoppingCart();
-            Action add = () => cart.Add(new Product(), -3);
-            add.Should().ThrowExactly<NegativeQuantity>().WithMessage("-3 is an invalid quantity.");
+            Action add = () => cart.Add(new Product(), quantity);
+            add.Should().ThrowExactly<NegativeQuantity>().WithMessage($"{quantity} is an invalid quantity.");
         }
     }
 
