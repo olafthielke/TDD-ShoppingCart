8324365e16ad5f65e6fbba572f73d35f105eff61
Olaf Thielke <olaf.thielke@gmail.com>
2022-01-01 15:01:53 +1300

    Add 5 Bananas & Check Cart Total - Green

--- BusinessLogic/Product.cs
+++ BusinessLogic/Product.cs
@@ -3,10 +3,12 @@
     public class Product
     {
         public string Name { get; }
+        public decimal UnitPrice { get; }
 
         public Product(string name, decimal unitPrice)
         {
             Name = name;
+            UnitPrice = unitPrice;
         }
     }
 }
