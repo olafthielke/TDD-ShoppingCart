d2eb30d7efe81a7f16faac3342e68af558665d8a
Olaf Thielke <olaf.thielke@gmail.com>
2022-01-03 19:04:47 +1300

    Add Subtotal to ShoppingCartItem - Red (Compile)

--- BusinessLogic/ShoppingCartItem.cs
+++ BusinessLogic/ShoppingCartItem.cs
@@ -5,6 +5,7 @@
         public Product Product { get; }
         public int Quantity { get; }
         public string ProductName => Product.Name;
+        public decimal UnitPrice => Product.UnitPrice;
 
         public ShoppingCartItem(Product product, int quantity)
         {
