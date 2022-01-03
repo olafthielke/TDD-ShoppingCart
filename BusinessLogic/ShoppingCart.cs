ddd15221df20b15c9ea9c584ba3f2ed226ac5bb4
Olaf Thielke <olaf.thielke@gmail.com>
2022-01-03 19:06:30 +1300

    Add Subtotal to ShoppingCartItem - Green

--- BusinessLogic/ShoppingCart.cs
+++ BusinessLogic/ShoppingCart.cs
@@ -5,7 +5,7 @@ namespace BusinessLogic
     public class ShoppingCart
     {
         public IList<ShoppingCartItem> Items { get; } = new List<ShoppingCartItem>();
-        public decimal Total => Items.Any() ? Items[0].Quantity * Items[0].UnitPrice : 0;
+        public decimal Total => Items.Any() ? Items[0].Subtotal : 0;
         public int ItemsCount => Items.Count;
 
         public void Add(Product product, int quantity)
--- BusinessLogic/ShoppingCartItem.cs
+++ BusinessLogic/ShoppingCartItem.cs
@@ -6,6 +6,7 @@
         public int Quantity { get; }
         public string ProductName => Product.Name;
         public decimal UnitPrice => Product.UnitPrice;
+        public decimal Subtotal => Quantity * UnitPrice;
 
         public ShoppingCartItem(Product product, int quantity)
         {
